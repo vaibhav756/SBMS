cb3737b779b3a73c5f21bb3c3a6897a9e1eebd2f
Vaibhav Yadav <yvaibhav756@gmail.com>
2023-12-09 16:54:00 +0530

    Added Tightly coupling class with IS-A and HAS-A relation

--- 01-ObjectTightCouplingApplication/src/com/Car.java
+++ 01-ObjectTightCouplingApplication/src/com/Car.java
@@ -1,5 +1,6 @@
 package com;
-
+/*
+ * Tightly coupling with IS-A relation
 public class Car extends Engine{
 
 	public void drive()
@@ -11,4 +12,17 @@ public class Car extends Engine{
 		System.out.println("Engine Trouble...");	
 	}
 	
+}*/
+//Tightly coupling with HAS-A relation
+public class Car
+{
+	public void drive()
+	{
+		Engine eng=new Engine();
+		int status=eng.start();
+		if(status>0)
+		System.out.println("Car Started...");
+		else
+		System.out.println("Engine Trouble...");
+	}
 }
