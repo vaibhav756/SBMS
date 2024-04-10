8817d9a65909b5a28aa149e610288fd4342032c3
Vaibhav Yadav <yvaibhav756@gmail.com>
2024-04-10 18:31:29 +0530

    ClientId and ClientSecret added from local .properties file

--- 66-Spring-Security-OAuth-App/src/main/java/com/Application.java
+++ 66-Spring-Security-OAuth-App/src/main/java/com/Application.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:C:\\Java\\SBMS\\OAuth2Credentials.properties")
 public class Application {
 
 	public static void main(String[] args) {
